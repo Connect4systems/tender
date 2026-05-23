967370d89e607a4fd87cc70fda2602a052252d39
Connect4 <connect4systems@gmail.com>
2026-05-23 14:17:50 +0300

    style

--- tender/hooks.py
+++ tender/hooks.py
@@ -25,7 +25,7 @@ app_license = "mit"
 # ------------------
 
 # include js, css files in header of desk.html
-# app_include_css = "/assets/tender/css/tender.css"
+app_include_css = "/assets/tender/css/tender_form.css"
 # app_include_js = "/assets/tender/js/tender.js"
 
 # include js, css files in header of web template
--- tender/tender/doctype/tender/tender.js
+++ tender/tender/doctype/tender/tender.js
@@ -1,8 +1,43 @@
 // Copyright (c) 2026, Connect4Systems and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Tender", {
-// 	refresh(frm) {
+frappe.ui.form.on("Tender", {
+	refresh(frm) {
+		frm.$wrapper.addClass("tender-form-shell");
+		tender_style_tabs(frm);
+	},
+	onload_post_render(frm) {
+		frm.$wrapper.addClass("tender-form-shell");
+		tender_style_tabs(frm);
+	},
+});
 
-// 	},
-// });
+function tender_style_tabs(frm) {
+	const icons = {
+		"Basic Information": "fa-th-large",
+		"Addresses and dates": "fa-clock-o",
+		"Classification and Execution": "fa-list",
+		"Quantity Tables": "fa-table",
+		"Attachments": "fa-paperclip",
+		"Competition News": "fa-newspaper-o",
+		"Local Content Mechanisms": "fa-home",
+		"Evaluation Criteria": "fa-university",
+		"المعلومات الأساسية": "fa-th-large",
+		"العناوين والمواعيد المتعلقة بالمنافسة": "fa-clock-o",
+		"مجال التصنيف وموقع التنفيذ والتقديم": "fa-list",
+		"جداول الكميات": "fa-table",
+		"المرفقات": "fa-paperclip",
+		"أخبار المنافسة": "fa-newspaper-o",
+		"آليات المحتوى المحلي": "fa-home",
+		"معايير التقييم": "fa-university",
+	};
+
+	frm.$wrapper.find(".form-tabs a.nav-link, .form-tabs a").each(function () {
+		const $tab = $(this);
+		const label = $tab.text().trim();
+
+		if (!$tab.find(".tender-tab-icon").length && icons[label]) {
+			$tab.prepend(`<i class="fa ${icons[label]} tender-tab-icon" aria-hidden="true"></i>`);
+		}
+	});
+}
