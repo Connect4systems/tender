bf6562688d51eb86ba277ca60ba686f32e2b419e
Connect4 <connect4systems@gmail.com>
2026-05-23 14:12:11 +0300

    1st tender

---
+++ tender/tender/doctype/tender_quantity_item/tender_quantity_item.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2026, Connect4Systems and contributors
+# For license information, please see license.txt
+
+from frappe.model.document import Document
+
+
+class TenderQuantityItem(Document):
+	pass
---
+++ tender/tender/doctype/tender_quantity_item/test_tender_quantity_item.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2026, Connect4Systems and contributors
+# For license information, please see license.txt
+
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestTenderQuantityItem(FrappeTestCase):
+	pass
